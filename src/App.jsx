22810e1e4c17af004ca978eb370c9051580025c0
cara <cara656513@gmail.com>
2024-10-30 12:19:45 +0900

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>2024 파리 올림픽</h2>
+      <div style={{
+        display: "flex",
+        gap: "10px",
+      }}>
+        <div><h3>국가명</h3>
+          <input type="text" />
+        </div>
+        <div><h3>금메달</h3>
+          <input type="number" />
+        </div>
+        <div><h3>은메달</h3>
+          <input type="number" />
+        </div>
+        <div><h3>동메달</h3>
+          <input type="number" />
+        </div>
+        <div>
+          <button>국가 추가</button>
+          <button>업데이트</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
