4e2da82e554e0a9133ce15e40203f2b3935f63cb
cara <cara656513@gmail.com>
2024-10-30 16:00:12 +0900

    css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,75 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nation, setNation] = useState([]);
+  const addNationHandler = () => {
+    const newNation = {
+      name: nation
+    }
+
+    setNation([...nation], newNation)
+  }
 
   return (
     <>
-      <h2>2024 파리 올림픽</h2>
       <div style={{
-        display: "flex",
-        gap: "10px",
+        display: "grid",
+        justifyContent: "center"
       }}>
-        <div><h3>국가명</h3>
-          <input type="text" />
+        <div style={{
+          textAlign: "center",
+          display: "grid",
+          justifyContent: "center",
+          margin: "40px 0 0 0",
+          padding: "30px",
+          backgroundColor: "white",
+          width: "1000px",
+          borderRadius: "10px",
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+        }}>
+          <h2>2024 파리 올림픽</h2>
+          <div style={{
+            display: "flex",
+            gap: "10px",
+          }}>
+            <div><h3>국가명</h3>
+              <input
+                type="text"
+                onChange={(e) => setNation(e.target.value)}
+              />
+            </div>
+            <div><h3>금메달</h3>
+              <input type="number" />
+            </div>
+            <div><h3>은메달</h3>
+              <input type="number" />
+            </div>
+            <div><h3>동메달</h3>
+              <input type="number" />
+            </div>
+            <div>
+              <button
+                onClick={addNationHandler}
+              >국가 추가</button>
+              <button>업데이트</button>
+            </div>
+          </div>
         </div>
-        <div><h3>금메달</h3>
-          <input type="number" />
-        </div>
-        <div><h3>은메달</h3>
-          <input type="number" />
-        </div>
-        <div><h3>동메달</h3>
-          <input type="number" />
-        </div>
-        <div>
-          <button>국가 추가</button>
-          <button>업데이트</button>
+        <div style={{
+          textAlign: "center",
+          display: "grid",
+          justifyContent: "center",
+          margin: "40px 0 0 0",
+          padding: "30px",
+          backgroundColor: "white",
+          width: "1000px",
+          borderRadius: "10px",
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+        }}>
+          <div>
+            아직 추가된 국가가 없습니다. 메달을 추적하세요!
+
+          </div>
         </div>
       </div>
     </>
