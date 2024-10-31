c09d7e64aaaab3633cffe26e371fa77d1e346a7b
cara <cara656513@gmail.com>
2024-10-31 14:37:32 +0900

    정렬 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Info from './components/Info';
 
 function App() {
   const style = {
@@ -13,13 +14,13 @@ function App() {
     boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
   }
 
+  const indexStyle = {
+    width: "100px"
+  }
+
   const alignStyle = {
     display: "flex",
     gap: "10px"
-  }
-
-  const indexStyle = {
-    width: "100px"
   }
 
   const [allInfo, setAllInfo] = useState([]);
@@ -28,6 +29,8 @@ function App() {
   const [gold, setGold] = useState(0);
   const [silver, setSilver] = useState(0);
   const [bronze, setBronze] = useState(0);
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -40,14 +43,19 @@ function App() {
       bronze: Number(bronze)
     }
 
-    if (!newInfo.nation.trim()) {
+    if (!newInfo.nation) {
       alert('국가명을 입력하세요');
       return;
     } else if (allInfo.map(info => info.nation).includes(newInfo.nation)) {
       alert('이미 존재하는 국가명입니다');
       return;
     }
-    setAllInfo([...allInfo, newInfo])
+
+    setAllInfo([...allInfo, newInfo]);
+    setNation("");
+    setGold("");
+    setSilver("");
+    setBronze("");
 
   }
 
@@ -94,6 +102,10 @@ function App() {
     if (!allInfo.map((info) => info.nation).includes(editedInfo.nation)) {
     }
 
+    setNation("");
+    setGold("");
+    setSilver("");
+    setBronze("");
   }
 
   return (
@@ -118,9 +130,16 @@ function App() {
                   }}
                 />
               </div>
-              <div><h3>금메달</h3>
+              <div
+
+              ><h3>금메달</h3>
                 <input
+                  style={{
+                    width: "150px"
+                  }}
                   type="number"
+                  min="0"
+                  max="99"
                   value={gold}
                   onChange={(e) => {
                     setGold(e.target.value)
@@ -129,7 +148,12 @@ function App() {
               </div>
               <div><h3>은메달</h3>
                 <input
+                  style={{
+                    width: "150px"
+                  }}
                   type="number"
+                  min="0"
+                  max="99"
                   value={silver}
                   onChange={(e) => {
                     setSilver(e.target.value)
@@ -138,7 +162,12 @@ function App() {
               </div>
               <div><h3>동메달</h3>
                 <input
+                  style={{
+                    width: "150px"
+                  }}
                   type="number"
+                  min="0"
+                  max="99"
                   value={bronze}
                   onChange={(e) => {
                     setBronze(e.target.value)
@@ -175,26 +204,12 @@ function App() {
               <div style={indexStyle}>액션</div>
             </div>
           </div>
-          {allInfo.map(function (info) {
-            return (
-              <div key={info.id} style={alignStyle}>
-                <div>
-                  <div style={indexStyle}>{info.nation}</div>
-                </div>
-                <div>
-                  <div style={indexStyle}>{info.gold}</div>
-                </div>
-                <div>
-                  <div style={indexStyle}>{info.silver}</div>
-                </div>
-                <div>
-                  <div style={indexStyle}>{info.bronze}</div>
-                </div>
-                <div style={indexStyle}>
-                  <button onClick={() => handleDelete(info.id)}>삭제</button>
-                </div>
-              </div>
-            )
+          {allInfo.sort((a, b) => b.gold - a.gold).map(function (info) {
+            return <Info
+              key={info.id}
+              info={info}
+              handleDelete={handleDelete}
+            />
           })}
         </div>
       </div>
@@ -204,3 +219,4 @@ function App() {
 }
 
 export default App
+
